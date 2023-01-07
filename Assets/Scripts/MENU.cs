597008b97641b7837caeda0635e7dd843f173486
Miguel Santos Oliveira <miguel.santos321_@hotmail.com>
2023-01-06 23:56:02 -0300

    menu test added

---
+++ Assets/Scripts/MENU.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MENU : MonoBehaviour
+{
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    
+
+}
