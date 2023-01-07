76e0b68abaaedbf0884d3a500d36e314b550ef96
Gustavo Silva <xgustavossilva@gmail.com>
2023-01-07 17:15:37 -0300

    Add generic singleton function

---
+++ Assets/Scripts/Utils/Singleton.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<T>();
+            }
+            return _instance;
+        }
+
+        private set
+        {
+            _instance = value;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
